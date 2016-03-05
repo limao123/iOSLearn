8d7190e96cd3e560c3d222953cabbecb14355aaa
limaofuyuanzhang <397524331@qq.com>
2016-03-05 11:13:45 +0800

    15-SlowWorker GCD的简单使用

---
+++ 15-SlowWorker/15-SlowWorker/ViewController.m
@@ -0,0 +1,75 @@
+//
+//  ViewController.m
+//  15-SlowWorker
+//
+//  Created by 林涛 on 16/3/5.
+//  Copyright © 2016年 limaofuyuanzhang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIButton *startButton;
+@property (weak, nonatomic) IBOutlet UITextView *resultsTextView;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *spinner;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - 执行的多个任务
+- (NSString *)fetchSomethingFromServer{
+    [NSThread sleepForTimeInterval:1];
+    return @"Hi there";
+}
+
+- (NSString *)processData:(NSString *)data{
+    [NSThread sleepForTimeInterval:2];
+    return [data uppercaseString];
+}
+
+- (NSString *)calculateFirstResult:(NSString *)data{
+    [NSThread sleepForTimeInterval:3];
+    return [NSString stringWithFormat:@"Number of chars:%d",[data length]];
+}
+
+- (NSString *)calculateSecondResult:(NSString *)data{
+    [NSThread sleepForTimeInterval:4];
+    return [data stringByReplacingOccurrencesOfString:@"E" withString:@"e"];
+}
+
+- (IBAction)doWork:(id)sender{
+    NSDate *startTime = [NSDate date];
+    self.startButton.enabled = NO;
+    [self.spinner startAnimating];
+    //使用默认队列来执行新的任务
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        //将这些耗时的任务都放在默认队列中，这样就不会阻塞主线程
+        NSString *fetchedData = [self fetchSomethingFromServer];
+        NSString *processedData = [self processData:fetchedData];
+        NSString *firstResult = [self calculateFirstResult:processedData];
+        NSString *secontResult = [self calculateSecondResult:processedData];
+        NSString *resultSummary = [NSString stringWithFormat:@"First:[%@]\nSecond:[%@]",firstResult,secontResult];
+        //UI操作需要使用主线程，在上述任务完成后，转回主线程
+        dispatch_async(dispatch_get_main_queue(), ^{
+            self.resultsTextView.text = resultSummary;
+            self.startButton = NO;
+            [self.spinner stopAnimating];
+        });
+        NSDate *endTime = [NSDate date];
+        NSLog(@"Completed in %f seconds",[endTime timeIntervalSinceDate:startTime]);
+    });
+
+}
+
+@end
