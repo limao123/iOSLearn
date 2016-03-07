8acf9bf60c24903bf475489c26a3923553f41af6
limaofuyuanzhang <397524331@qq.com>
2016-03-07 20:20:11 +0800

    15-StateLab后台处理

---
+++ 15-StateLab/15-StateLab/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  15-StateLab
+//
+//  Created by 林涛 on 16/3/7.
+//  Copyright © 2016年 limaofuyuanzhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 15-StateLab/15-StateLab/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  15-StateLab
+//
+//  Created by 林涛 on 16/3/7.
+//  Copyright © 2016年 limaofuyuanzhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
