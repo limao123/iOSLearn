fcb60068957f13cdcd4d0fd37536916115c45d5e
limaofuyuanzhang <397524331@qq.com>
2016-03-08 16:51:08 +0800

    测试

--- 15-StateLab/15-StateLab/ViewController.m
+++ 15-StateLab/15-StateLab/ViewController.m
@@ -9,14 +9,44 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (strong, nonatomic) UILabel *label;
 @end
 
-@implementation ViewController
+@implementation ViewController{
+    BOOL animate;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    CGRect bounds = self.view.bounds;
+    CGRect labelFrame = CGRectMake(bounds.origin.x, CGRectGetMidY(bounds)-50, bounds.size.width,100);
+    self.label = [[UILabel alloc] initWithFrame:labelFrame];
+    self.label.font = [UIFont fontWithName:@"Helvetica" size:70];
+    self.label.text = @"Bazinga!";
+    self.label.textAlignment = NSTextAlignmentCenter;
+    self.label.backgroundColor = [UIColor clearColor];
+    [self.view addSubview:self.label];
+    [self rotateLabelDown];
+    NSNotificationCenter *center = [NSNotificationCenter defaultCenter];
+    [center addObserver:self selector:@selector() name:UIApplicationWillResignActiveNotification object:nil];
+    
+}
+
+- (void)rotateLabelDown{
+    [UIView animateWithDuration:0.5 animations:^{
+        self.label.transform = CGAffineTransformMakeRotation(M_PI);
+    } completion:^(BOOL finished) {
+        [self rotateLabelUp];
+    }];
+}
+
+- (void)rotateLabelUp{
+    [UIView animateWithDuration:0.5 animations:^{
+        self.label.transform = CGAffineTransformMakeRotation(0);
+    } completion:^(BOOL finished) {
+        [self rotateLabelDown];
+    }];
 }
 
 - (void)didReceiveMemoryWarning {
