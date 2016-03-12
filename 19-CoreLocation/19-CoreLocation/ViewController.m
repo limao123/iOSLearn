6ea2a8e3dfdfffa43eba54adc8071349a0ae3e9b
limaofuyuanzhang <397524331@qq.com>
2016-03-12 20:39:06 +0800

    19-CoreLoaction练习

---
+++ 19-CoreLocation/19-CoreLocation/ViewController.m
@@ -0,0 +1,124 @@
+//
+//  ViewController.m
+//  19-CoreLocation
+//
+//  Created by bmob on 16/3/11.
+//  Copyright © 2016年 bmob. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <CoreLocation/CoreLocation.h>
+
+@interface ViewController ()<CLLocationManagerDelegate>
+@property (strong, nonatomic) CLLocationManager *locationManager;
+@property (strong, nonatomic) CLLocation *previousPoint;
+@property (assign, nonatomic) CLLocationDistance totalMovementDistance;
+@property (weak, nonatomic) IBOutlet UILabel *latitudeLabel;
+@property (weak, nonatomic) IBOutlet UILabel *longtitudeLabel;
+@property (weak, nonatomic) IBOutlet UILabel *horizontalAccuracyLabel;
+@property (weak, nonatomic) IBOutlet UILabel *altitudeLabel;
+@property (weak, nonatomic) IBOutlet UILabel *verticalAccuracyLabel;
+@property (weak, nonatomic) IBOutlet UILabel *distanceTraveledLabel;
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    //使用地理位置的基础设置
+    self.locationManager = [[CLLocationManager alloc] init];
+    self.locationManager.delegate = self;
+    //表示提供最佳精度
+    self.locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+    [self.locationManager startUpdatingLocation];
+}
+
+#pragma mark - CLLocationManagerDelegate Methods
+- (void)locationManager:(CLLocationManager *)manager
+     didUpdateLocations:(NSArray *)locations {
+    
+    //由于地理位置可能一次上报多个变化，因此最后一个才是当前的地理位置
+    CLLocation *newLocation = [locations lastObject];
+    
+    //显示当前位置信息
+    NSString *latitudeString = [NSString stringWithFormat:@"%g\u00B0",
+                                newLocation.coordinate.latitude];
+    self.latitudeLabel.text = latitudeString;
+    
+    NSString *longitudeString = [NSString stringWithFormat:@"%g\u00B0",
+                                 newLocation.coordinate.longitude];
+    self.longitudeLabel.text = longitudeString;
+    
+    NSString *horizontalAccuracyString = [NSString stringWithFormat:@"%gm",
+                                          newLocation.horizontalAccuracy];
+    self.horizontalAccuracyLabel.text = horizontalAccuracyString;
+    
+    NSString *altitudeString = [NSString stringWithFormat:@"%gm",
+                                newLocation.altitude];
+    self.altitudeLabel.text = altitudeString;
+    
+    NSString *verticalAccuracyString = [NSString stringWithFormat:@"%gm",
+                                        newLocation.verticalAccuracy];
+    self.verticalAccuracyLabel.text = verticalAccuracyString;
+    
+    if (newLocation.verticalAccuracy < 0 ||
+        newLocation.horizontalAccuracy < 0) {
+        // invalid accuracy
+        return;
+    }
+    
+    if (newLocation.horizontalAccuracy > 100 ||
+        newLocation.verticalAccuracy > 50) {
+        // accuracy radius is so large, we don't want to use it
+        return;
+    }
+    
+    if (self.previousPoint == nil) {
+        self.totalMovementDistance = 0;
+        
+        BIDPlace *start = [[BIDPlace alloc] init];
+        start.coordinate = newLocation.coordinate;
+        start.title = @"Start Point";
+        start.subtitle = @"This is where we started!";
+        
+        [self.mapView addAnnotation:start];
+        MKCoordinateRegion region;
+        region = MKCoordinateRegionMakeWithDistance(newLocation.coordinate,
+                                                    100, 100);
+        [self.mapView setRegion:region animated:YES];
+    } else {
+        NSLog(@"movement distance: %f", [newLocation
+                                         distanceFromLocation:self.previousPoint]);
+        
+        self.totalMovementDistance += [newLocation
+                                       distanceFromLocation:self.previousPoint];
+    }
+    self.previousPoint = newLocation;
+    
+    NSString *distanceString = [NSString stringWithFormat:@"%gm",
+                                self.totalMovementDistance];
+    self.distanceTraveledLabel.text = distanceString;
+    
+}
+
+//处理错误信息
+- (void)locationManager:(CLLocationManager *)manager
+       didFailWithError:(NSError *)error {
+    NSString *errorType = (error.code == kCLErrorDenied) ?
+    @"Access Denied" : @"Unknown Error";
+    UIAlertView *alert = [[UIAlertView alloc]
+                          initWithTitle:@"Error getting Location"
+                          message:errorType
+                          delegate:nil
+                          cancelButtonTitle:@"Okay"
+                          otherButtonTitles:nil];
+    [alert show];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ 19-CoreLocation/19-CoreLocation/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  19-CoreLocation
+//
+//  Created by bmob on 16/3/11.
+//  Copyright © 2016年 bmob. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
