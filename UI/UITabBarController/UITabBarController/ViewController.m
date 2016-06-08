09b4b7c8dcd31b261c53b59eadfa76e9770e2b44
limaofuyuanzhang <397524331@qq.com>
2016-06-08 18:01:43 +0800

    2016-06-08 company

---
+++ UI/UITabBarController/UITabBarController/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  UITabBarController
+//
+//  Created by bmob-LT on 16/6/8.
+//  Copyright © 2016年 bmob-LT. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    self.tabBarItem.title = @"vc";
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
