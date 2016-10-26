67ac2154e5adb908adb34a0af0beeed2bd80e18b
limaofuyuanzhang <397524331@qq.com>
2016-10-26 18:06:30 +0800

    10-26

---
+++ LTKit/LTLab/BK15Model.h
@@ -0,0 +1,13 @@
+//
+//  BK15Model.h
+//  LTKit
+//
+//  Created by bmob-LT on 2016/10/26.
+//  Copyright © 2016年 bmob-LT. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BK15Model : NSObject
+@property (nonatomic,readwrite,copy) NSString *name;
+@end
---
+++ LTKit/LTLab/BK15Model.m
@@ -0,0 +1,30 @@
+
+//
+//  BK15Model.m
+//  LTKit
+//
+//  Created by bmob-LT on 2016/10/26.
+//  Copyright © 2016年 bmob-LT. All rights reserved.
+//
+
+#import "BK15Model.h"
+
+@interface BK15Model ()
+
+@end
+
+
+
+@implementation BK15Model
+
+//@synthesize name = _name;
+
+- (void)setName:(NSString *)name {
+    _name = name;
+}
+
+- (NSString *)name {
+    return _name;
+}
+
+@end
